f4195134c32d24852d75ce948bde7f0021da01b3
Rishab N T <rishab698.n@gmail.com>
2023-06-11 22:13:34 +0530

    Fixed renames

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,12 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
     ); 
+    res.header(
+        'Access-Control-Allow-Methods', 
+        'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    );
     next();
 });
 
